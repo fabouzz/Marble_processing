050e236ed66da0530bdccdf203d84f4a183e9963
Fabien <fabien.depoilly@gmail.com>
2019-05-14 11:08:14 +0200

    Background substraction tests started

---
+++ ProcessingTest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Background substraction test."""
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+os.system("clear")
+
+plt.rc("font", family="serif")
