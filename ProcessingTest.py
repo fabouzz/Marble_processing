3b142934d73e6b787b2f887a1c8f930efcad6cb1
Fabien <fabien.depoilly@gmail.com>
2019-05-14 14:31:17 +0200

    Background substraction test continuing

--- ProcessingTest.py
+++ ProcessingTest.py
@@ -1,10 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Background substraction test."""
-import numpy as np
-import matplotlib.pyplot as plt
-import os
+"""Video background substraction test."""
 import cv2
-os.system("clear")
+import matplotlib.pyplot as plt
 
-plt.rc("font", family="serif")
+path = "/home/fabouzz/Cours/Projet_CMI_bille/mesuresBille/"
+videoName = 'test_cam6.avi'
+cap = cv2.VideoCapture(path + videoName)
+
+# Background substractor MO2
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+# Background substractor GMG
+# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+# fgbg = cv2.createBackgroundSubtractorGMG()
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    fgmask = fgbg.apply(frame)
+    cv2.imshow('frame', fgmask)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
