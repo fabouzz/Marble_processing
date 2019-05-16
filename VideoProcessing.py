cf62cf84060027dfb1162b98aaac9303fb9449ea
Fabien <fabien.depoilly@gmail.com>
2019-05-16 11:25:03 +0200

    Tool for frame visualisation + upgraded fgmask

--- VideoProcessing.py
+++ VideoProcessing.py
@@ -1,4 +1,4 @@
-"""Video background substraction test."""
+"""Video background substraction."""
 import cv2
 import matplotlib.pyplot as plt
 
@@ -6,16 +6,16 @@ path = "/home/fabouzz/Cours/Projet_CMI_bille/mesuresBille/"
 videoName = 'test_cam6.avi'
 cap = cv2.VideoCapture(path + videoName)
 
-filename = "VideoTest.avi"
-cap = cv2.VideoCapture(filename)
+# filename = "VideoTest.avi"
+# cap = cv2.VideoCapture(filename)
 
 # Background substractor MO2
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 # Background substractor GMG
 # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-# fgbg = cv2.createBackgroundSubtractorGMG()
-
+fgbg = cv2.createBackgroundSubtractorKNN()
+fig, ax = plt.subplots()
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -24,10 +24,9 @@ while(True):
     fgmask = fgbg.apply(frame)
 
     # Lisser l'image pour enlever du bruit
-    thresh = cv2.erode(fgmask, None, iterations=2)
+    # thresh = cv2.erode(fgmask, None, iterations=1)
     # thresh = cv2.dilate(thresh, None, iterations=4)
-    cv2.imshow('frame', thresh)
-
+    cv2.imshow('frame', fgmask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
