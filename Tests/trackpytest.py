94fe3d8a566e5bea2b56433623b65c15d0be7060
OupsMajDsl <oupsmajdsl@gmail.com>
2019-05-17 16:01:34 +0200

    testin trackpy library

--- Tests/trackpytest.py
+++ Tests/trackpytest.py
@@ -9,7 +9,7 @@ import pandas as pd
 import pims
 import trackpy as tp
 
-datapath = "Tests/"
+datapath = "/home/mathieu/Documents/S4/ProjetBille/VideoProcessing/images/"
 filename = "screen_bulles.png"
 
 def crop(img):
@@ -22,7 +22,5 @@ def crop(img):
     y_max = 672
     return img[y_min:y_max,x_min:x_max]
 
-rawframes = pims.ImageSequence(datapath + filename)
-
-
-
+frame = pims.ImageSequence(datapath + filename, process_func=crop)
+plt.imshow(frame)
