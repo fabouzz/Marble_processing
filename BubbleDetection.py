bf856f1d0778beac4ab2d4f188dc4e3447e7df20
OupsMajDsl <oupsmajdsl@gmail.com>
2019-05-17 13:16:43 +0200

    bubble detection working better image filtering needed

--- BubbleDetection.py
+++ BubbleDetection.py
@@ -1,15 +1,17 @@
+"""
+Détection de bulles sur une image 
+"""
+
 from imutils import contours
 from skimage import measure
 import numpy as np
-import scipy.ndimage
-import argparse
 import imutils
 import cv2
 
 IMAGE_PATH = "screen_bulles.png"
-IMAGE = cv2.imread(IMAGE_PATH)
+ORIG_IMAGE = cv2.imread(IMAGE_PATH)
 # Pour éviter d'avoir 3 channels RGB identiques
-IMAGE = cv2.cvtColor(IMAGE, cv2.COLOR_BGR2GRAY)
+IMAGE = cv2.cvtColor(ORIG_IMAGE, cv2.COLOR_BGR2GRAY)
 
 # Récupération de la taille de l'image pour crop
 height, width = IMAGE.shape
@@ -18,14 +20,15 @@ heightMax = height
 widthMin = 330
 widthMax = 700
 
-IMAGE = IMAGE[heightMin:heightMax, widthMin:widthMax]
+IMAGE_NEW = IMAGE[heightMin:heightMax, widthMin:widthMax]
 
 # Lissage de l'image
-thresh = cv2.erode(IMAGE, None, iterations=2)
+thresh = cv2.erode(IMAGE_NEW, None, iterations=2)
 thresh = cv2.dilate(thresh, None, iterations=4)
 
-# Limite coupure pour binariser l'image 
+# Limite de coupure pour binariser l'image 
 LimThresh = 75
+# Traitement du seuil pour toute l'image
 for x in range(thresh.shape[0]):
     for y in range(thresh.shape[1]):
         # Si pixel trop clair, il devient blanc
@@ -36,14 +39,30 @@ for x in range(thresh.shape[0]):
 # =======
 # Fin du traitement de l'image
 
-contours, h = cv2.findContours(thresh, 1, 2)
-nb_bulles = len(contours)
-print(nb_bulles)
+# Identification des contours
+contours= cv2.findContours(thresh, 1, 2)
+cnts = imutils.grab_contours(contours)
+nb_bulles = len(cnts)
 
-thresh_rgb = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-for cnt in contours:
-  cv2.drawContours(thresh_rgb, [cnt], 0, (0, 0, 255), 2)
+
+# creating empty lists to save centers of each bubble
+xpos = []
+ypos = []
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	# resize centers of the bubbles to fit original image
+	cX = int(M["m10"] / M["m00"]) + widthMin
+	cY = int(M["m01"] / M["m00"]) + heightMin
+	# Place a circle on the center of the bubble
+	cv2.circle(ORIG_IMAGE, (cX, cY), 3, (0, 0, 255), -1)
+
+# display number of bubbles
+print(nb_bulles)
+cv2.imwrite("exampleimage.png", ORIG_IMAGE)
 
 if __name__ == "__main__":
-    cv2.imshow("treated image", thresh_rgb)
+    cv2.imshow("treated image", ORIG_IMAGE)
     cv2.waitKey(0)
+	
--- BubbleDetectionOnVid.py
+++ BubbleDetectionOnVid.py
@@ -1,51 +1,72 @@
 from imutils import contours
 from skimage import measure
 import numpy as np
-import scipy.ndimage
-import argparse
 import imutils
 import cv2
-path = "/home/mathieu/Documents/S4/ProjetBille/VideoProcessing/"
-videoName = "VideoTest.avi"
+
+path = "/media/mathieu/EHDD/videos_bille/"
+videoName = "mes_haut5_bille3_1.avi"
 cap = cv2.VideoCapture(path + videoName)
-cap.set(cv2.CAP_PROP_POS_FRAMES, 200)
+cap.set(cv2.CAP_PROP_POS_FRAMES, 150)
 while cap.isOpened():
     ret, frame = cap.read()
 
-# Pour éviter d'avoir 3 channels RGB identiques
-    IMAGE = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Récupération de la taille de l'image pour crop
-    height, width = IMAGE.shape
-    # IMAGE = IMAGE[440:height, 330:700]
+    if ret:
+        IMAGE = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Récupération de la taille de l'image pour crop
+        height, width = IMAGE.shape
+        heightMin = 370
+        heightMax = height
+        widthMin = 300
+        widthMax = 860
 
-    # Lissage de l'image
-    thresh = cv2.erode(IMAGE, None, iterations=2)
-    thresh = cv2.dilate(thresh, None, iterations=4)
+        IMAGE_NEW = IMAGE[heightMin:heightMax, widthMin:widthMax]
 
-    # Limite coupure pour binariser l'image 
-    LimThresh = 75
-    for x in range(thresh.shape[0]):
-        for y in range(thresh.shape[1]):
-            # Si pixel trop clair, il devient blanc
-            if thresh[x, y] > LimThresh:
-                thresh[x, y] = 255
-            elif thresh[x, y] <= LimThresh:
-                thresh[x, y] = 0
-    # =======
-    # Fin du traitement de l'image
+        # Lissage de l'image
+        thresh = cv2.erode(IMAGE_NEW, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=4)
 
-    contours, h = cv2.findContours(thresh, 1, 2)
-    nb_bulles = len(contours)
-    print(nb_bulles)
+        # Limite de coupure pour binariser l'image 
+        # Traitement et application du filtre
+        # LimThresh = 90
+        # # Traitement du seuil pour toute l'image
+        # for x in range(thresh.shape[0]):
+        #     for y in range(thresh.shape[1]):
+        #         # Si pixel trop clair, il devient blanc
+        #         if thresh[x, y] > LimThresh:
+        #             thresh[x, y] = 255
+        #         elif thresh[x, y] <= LimThresh:
+        #             thresh[x, y] = 0
+        # =======
+        # Fin du traitement de l'image
 
-    thresh_rgb = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-    for cnt in contours:
-        cv2.drawContours(thresh_rgb, [cnt], 0, (0, 0, 255), 2)
+        # Identification des contours
+        contours= cv2.findContours(thresh, 1, 2)
+        cnts = imutils.grab_contours(contours)
+        nb_bulles = len(cnts)
 
+        # creating empty lists to save centers of each bubble
+        xpos = []
+        ypos = []
+        # loop over the contours
+        for c in cnts:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            # resize centers of the bubbles to fit original image
+            cX = int(M["m10"] / M["m00"]) + widthMin
+            cY = int(M["m01"] / M["m00"]) + heightMin
+            # Place a circle on the center of the bubble
+            cv2.circle(frame, (cX, cY), 3, (0, 0, 255), -1)
+        
+        cv2.imshow("output", frame)
+        cv2.imshow("output2", thresh)
+        if cv2.waitKey(200) & 0xFF == ord('q'):
+            break
 
-    cv2.imshow("treated image", thresh_rgb)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    else:
         break
+
+
 
 # When everything done, release the capture
 cap.release()
