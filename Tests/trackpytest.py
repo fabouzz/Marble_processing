a02fcb3c476bf9ba0260fd4d21c38c8f3181d047
OupsMajDsl <oupsmajdsl@gmail.com>
2019-05-20 11:58:35 +0200

    filtering optimized

--- Tests/trackpytest.py
+++ Tests/trackpytest.py
@@ -3,14 +3,24 @@ Testing the trackpy library
 """
 import os
 import cv2
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 import pims
 import trackpy as tp
 from scipy import ndimage
 from skimage import filters, morphology, util
+from pims import pipeline
+
+@pipeline
+def as_grey(frame):
+    red = frame[:, :, 0]
+    green = frame[:, :, 1]
+    blue = frame[:, :, 2]
+    return 0.2125 * red + 0.7154 * green + 0.0721 * blue
 
 def crop(img):
     """
@@ -42,15 +52,26 @@ def preprocess_foam(img):
     img = ndimage.binary_dilation(img)
     return util.img_as_int(img)
 
-IMAGE_PATH = "../images/screen_bulles.png"
-ORIG_IMAGE = cv2.imread(IMAGE_PATH, cv2.IMREAD_GRAYSCALE)
-# Pour éviter d'avoir 3 channels RGB identiques
-IMAGE = preprocess_foam(ORIG_IMAGE)
-ORIG_IMAGE = crop(ORIG_IMAGE)
+# IMAGE_PATH = "../images/screen_bulles.png"
+# ORIG_IMAGE = cv2.imread(IMAGE_PATH, cv2.IMREAD_GRAYSCALE)
+# # Pour éviter d'avoir 3 channels RGB identiques
+# IMAGE = preprocess_foam(ORIG_IMAGE)
+# ORIG_IMAGE = crop(ORIG_IMAGE)
 
-fig, ax = plt.subplots(2, 1)
-ax[0].imshow(ORIG_IMAGE, cmap='Greys_r')
-ax[1].imshow(IMAGE, cmap='Greys')
+# fig, ax = plt.subplots(2, 1)
+# ax[0].imshow(ORIG_IMAGE, cmap='Greys_r')
+# ax[1].imshow(IMAGE, cmap='Greys')
+# plt.show()
+# ==========================================================
 
+fig, ax = plt.subplots()
+id_example = 200
+filepath = "/media/mathieu/EHDD/videos_bille/mes_haut5_bille2_2.avi"
+
+frames = pims.Video(filepath)
+frame_n = frames[id_example]
+frame = as_grey(frame_n)
+print(frame)
+ax.imshow(frame, cmap='Greys_r')
 
 plt.show()
