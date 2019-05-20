ebe3394dafd812c6b12660ca92c2a3571a4ed9d3
OupsMajDsl <oupsmajdsl@gmail.com>
2019-05-20 11:00:58 +0200

    bubble detection efficient with skimage filters

--- Tests/trackpytest.py
+++ Tests/trackpytest.py
@@ -2,25 +2,55 @@
 Testing the trackpy library
 """
 import os
+import cv2
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pims
 import trackpy as tp
-
-datapath = "/home/mathieu/Documents/S4/ProjetBille/VideoProcessing/images/"
-filename = "screen_bulles.png"
+from scipy import ndimage
+from skimage import filters, morphology, util
 
 def crop(img):
     """
     Crop the image to select the region of interest
     """
-    x_min = 330
-    x_max = 700
+    x_min = 300
+    x_max = 600
     y_min = 440
     y_max = 672
     return img[y_min:y_max,x_min:x_max]
 
-frame = pims.ImageSequence(datapath + filename, process_func=crop)
-plt.imshow(frame)
+def preprocess_foam(img):
+    """
+    Apply image processing functions to return a binary image
+    """
+    # Crop the pictures as for raw images.
+    img = crop(img)
+    # Apply thresholds
+    block_size = 5
+    img = filters.threshold_local(img, block_size)
+    threshold = 0.25
+    idx = img > img.max() * threshold
+    idx2 = img < img.max() * threshold
+    img[idx] = 0
+    img[idx2] = 255
+    # Dilatate to get a continous network
+    # of liquid films
+    img = ndimage.binary_dilation(img)
+    img = ndimage.binary_dilation(img)
+    return util.img_as_int(img)
+
+IMAGE_PATH = "../images/screen_bulles.png"
+ORIG_IMAGE = cv2.imread(IMAGE_PATH, cv2.IMREAD_GRAYSCALE)
+# Pour éviter d'avoir 3 channels RGB identiques
+IMAGE = preprocess_foam(ORIG_IMAGE)
+ORIG_IMAGE = crop(ORIG_IMAGE)
+
+fig, ax = plt.subplots(2, 1)
+ax[0].imshow(ORIG_IMAGE, cmap='Greys_r')
+ax[1].imshow(IMAGE, cmap='Greys')
+
+
+plt.show()
